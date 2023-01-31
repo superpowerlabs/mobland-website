f6e9ea3825b1509dc8c34b54fb52fa8f7ade0ed8
Francesco Sullo <sullof@gmail.com>
2023-01-31 10:45:24 -0800

    Update rate-limiter.js

--- server/applySecurity/rate-limiter.js
+++ server/applySecurity/rate-limiter.js
@@ -4,4 +4,9 @@ module.exports = rateLimiter({
   max: 60, // max number of requests a user can make within a specified timeframe
   windowMS: 10000, // 10 seconds
   message: "You can't make any more requests at the moment. Try again later",
+  keyGenerator: (req) => {
+    return req.headers["x-real-ip"] ||
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress;
+  },
 });
