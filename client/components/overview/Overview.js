9ab38ba59c01907c9da896584bbe193a33df4eec
Francesco Sullo <sullof@gmail.com>
2023-02-05 16:34:25 -0800

    All lowercase

--- client/components/overview/Overview.js
+++ client/components/overview/Overview.js
@@ -93,6 +93,7 @@ function Overview({ subsection }) {
       >
         <AssetsGuide />
       </Container>
+      <div style={{height: 100}}><span className={'transparent'}>-</span> </div>
     </div>
   );
 }
