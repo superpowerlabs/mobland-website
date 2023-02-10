a7641a4ef8a6201dcdd8f94ebb781e7cfa009439
jerrybassat7 <jerry.bassat@upr.edu>
2023-02-10 11:27:23 -0400

    Clean Images

--- client/components/overview/SeedFarmGuide.js
+++ client/components/overview/SeedFarmGuide.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid} from "@mui/material";
+import { Grid } from "@mui/material";
 import Exchanges from "./Exchanges";
 import NftExchange from "./NftExchange";
 
