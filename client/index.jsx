fcd59ef335c3fb59176babb09ce89e6a9350cdeb
roquesrolando <roquesrolando@hotmail.com>
2023-06-02 11:06:10 -0400

    added bootstrap

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
-import "../client/assets/css/style.css";
 import App from "./components/App";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../client/assets/css/style.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
